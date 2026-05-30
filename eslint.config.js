fbdd9ccb84e7eee21ffbbc47c93ac859a9aa524c
Tomasz Pietrzyk <t.pietrzyk@yahoo.pl>
2026-05-30 17:22:26 +0200

    [#45] Add ESLint and Prettier setup

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js"
+import globals from "globals"
+import prettier from "eslint-config-prettier"
+
+export default [
+  { ignores: ["app/assets/builds/"] },
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  prettier
+]
