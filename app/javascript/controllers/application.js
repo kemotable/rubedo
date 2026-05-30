8518038d2b5459e04ecf438b02a502f2a87d8a0c
Tomasz Pietrzyk <t.pietrzyk@yahoo.pl>
2026-05-30 17:55:52 +0200

    Add ESLint, Prettier setup (#46)
    * [#45] Add ESLint and Prettier setup
    
    * [#45] Address CR feedback: extend lint/format scope to eslint.config.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,6 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
-application.debug = process.env.NODE_ENV === "development"
+const application = Application.start();
+application.debug = process.env.NODE_ENV === "development";
 
-export { application }
+export { application };
